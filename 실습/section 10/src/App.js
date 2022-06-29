ea5369e1bd4b25d05724b7e9e32495d6e4d8b26d
jiyoung <remi1217@naver.com>
2022-06-30 01:20:10 +0900

    section 10

---
+++ 실습/section 10/src/App.js
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './store/auth-context';
+
+function App() {
+  const ctx = useContext(AuthContext);
+  return (
+    <React.Fragment>
+      <MainHeader />
+      <main>
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
